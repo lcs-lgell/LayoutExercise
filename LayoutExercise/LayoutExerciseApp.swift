e556333e92a91dda0daca9b9978b4653f764e63b
Leon Gell <lgell@lcs.on.ca>
2023-01-10 09:30:10 -0500

    Initial Commit

---
+++ LayoutExercise/LayoutExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExerciseApp.swift
+//  LayoutExercise
+//
+//  Created by Leon Gell on 2023-01-10.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
